1af8cc2e502db28e949da4d90013d72818aeba98
Britta Carlsson <guscarbr@student.gu.se>
2019-02-24 09:52:44 +0100

    added cs

--- simdoc.py
+++ simdoc.py
@@ -7,12 +7,45 @@ from sklearn.metrics.pairwise import cosine_similarity
 # simdoc.py -- Don't forget to put a reasonable amount code comments
 # in so that we better understand what you're doing when we grade!
 
-# add whatever additional imports you may need here
+def read_as_df(filename):
+    vectorfile_df = pd.read_csv(filename)
+    vectors = [doc[1:-1] for doc in vectorfile_df['vector']]
+    vectors = [np.fromstring(doc,sep=' ') for doc in vectors]
+    vectorfile_df['vector'] = vectors
+    
+    return vectorfile_df
 
-parser = argparse.ArgumentParser(description="Compute some similarity statistics.")
-parser.add_argument("vectorfile", type=str,
+def similarity_within_topic(vectorfile_df):
+    grain = vectorfile_df[vectorfile_df["subfolder"]=="grain"]
+    grain_vectors = [doc for doc in grain['vector']]
+    cs_grain = cosine_similarity(grain_vectors)
+    
+    crude = vectorfile_df[vectorfile_df["subfolder"]=="crude"]
+    crude_vectors = [doc for doc in crude['vector']]
+    cs_crude = cosine_similarity(crude_vectors)
+    
+    return cs_grain,cs_crude   
+    
+def similarity_between_topics(vectorfile_df):
+    grain = vectorfile_df[vectorfile_df["subfolder"]=="grain"]
+    grain_vectors = [doc for doc in grain['vector']]
+    
+    crude = vectorfile_df[vectorfile_df["subfolder"]=="crude"]
+    crude_vectors = [doc for doc in crude['vector']]
+    
+    cs_between = cosine_similarity(grain_vectors,crude_vectors)
+    
+    return cs_between
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Compute some similarity statistics.")
+    parser.add_argument("vectorfile", type=str,
                     help="The name of the input  file for the matrix data.")
 
-args = parser.parse_args()
+    args = parser.parse_args()
+    
+    df_vectorfile = read_as_df(args.vectorfile)
+    cosine_similarity_1 = similarity_within_topic(vectorfile_df)
 
-print("Reading matrix from {}.".format(args.vectorfile))
+    print("Reading matrix from {}.".format(args.vectorfile))
